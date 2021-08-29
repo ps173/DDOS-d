4d4f0cff190410fbbdb99d089cd1f4cb672e86a9
ps173 <prathamsharma173@gmail.com>
2021-08-30 01:40:53 +0530

    Initial Project

---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+ t.window.title = "DDOS'D"
+ t.window.width = 800
+ t.window.height = 640
+end
---
+++ main.lua
@@ -0,0 +1,9 @@
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+ love.graphics.print("Hellow World!",100,100)
+end
