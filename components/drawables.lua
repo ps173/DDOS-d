cc02464113b8fbaa1c498f50cc1e6209cf454ea2
ps173 <prathamsharma173@gmail.com>
2021-08-30 23:43:44 +0530

    enemy assets and draw

--- components/drawables.lua
+++ components/drawables.lua
@@ -1,12 +1,13 @@
 --[[
 Drawing Logic Gates
+TODO : ADD PARTICLE ANIMATIONS TO THESE
 --]]
 
 local objects = {}
 
 -- gates as drawables
 objects.gates = {}
--- @params gate : name of the gate i.e, and ,or
+-- @params gate(string) : name of the gate i.e, and/or
 function objects.gates.draw(gate,x,y) 
  local andimg = love.graphics.newImage("assets/and_gate.png")
  local orimg = love.graphics.newImage("assets/or_gate.png")
@@ -19,6 +20,21 @@ function objects.gates.draw(gate,x,y)
  if gate=="or" then
    love.graphics.setColor(0,1,0)
    love.graphics.draw(orimg,x,y)
+ end
+end
+
+objects.aimachines = {}
+-- @params name(string) : name of the aimachines i.e, executive,admiral,president
+function objects.aimachines.draw(name,x,y) 
+ local execimg = love.graphics.newImage("assets/exec_ai.png")
+ local admimg = love.graphics.newImage("assets/admir_ai.png")
+
+ if name=="admiral" then
+   love.graphics.draw(admimg,x,y)
+ end
+
+ if name=="executive" then
+   love.graphics.draw(execimg,x,y)
  end
 end
 
