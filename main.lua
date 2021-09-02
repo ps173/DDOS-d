11d7f7b353f63a2a995fd98570bb253c36f71e64
ps173 <prathamsharma173@gmail.com>
2021-09-02 15:49:37 +0530

    not something big

--- main.lua
+++ main.lua
@@ -1,21 +1,42 @@
-drawables = require"components/drawables"
+Drawables = require"components/drawables"
 LOGIC_GATES = require"utils/logic-gates"
 
 function love.load()
  -- demo level
  level = require"levels"
+ mousex = 0
+ mousey = 0
 end
 
 function love.update(dt)
+ -- Change the state of level 
 end
 
 function love.draw()
- drawables.aimachines.draw(
+ Drawables.aimachines.draw(
     level.aimachines.name,
     level.aimachines.x,
     level.aimachines.y)
- drawables.gates.draw(level.gates.name,level.gates.x,level.gates.y)
+ Drawables.gates.draw(level.gates.name,level.gates.x,level.gates.y)
  for y, row in ipairs(level.signator) do
-  drawables.signator.draw(row.state,row.x,row.y)
+  Drawables.signator.draw(row.state,row.x,row.y)
+ end
+
+
+ -- temp debug solutions
+love.graphics.print(mousex,100,10)
+love.graphics.print(mousey,300,10)
+
+end
+
+function love.mousepressed(x,y,button)
+ if button ==1 then
+  for y,table in ipairs(level.signator) do
+   if table.x == x or table.x +25 >= x and table.y == y or table.y +25 >= y then 
+    table.state = not table.state
+   end
+  end
+  mousex = x
+  mousey = y
  end
 end
