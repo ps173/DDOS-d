c69649f7373f292c197c374571a725322448f024
ps173 <prathamsharma173@gmail.com>
2021-09-02 16:43:23 +0530

    Commited Changes

--- main.lua
+++ main.lua
@@ -2,41 +2,41 @@ Drawables = require"components/drawables"
 LOGIC_GATES = require"utils/logic-gates"
 
 function love.load()
- -- demo level
- level = require"levels"
- mousex = 0
- mousey = 0
+ -- demo Level
+ Level = require"levels"
+ Mousex = 0
+ Mousey = 0
 end
 
 function love.update(dt)
- -- Change the state of level 
+ -- Change the state of Level
 end
 
 function love.draw()
  Drawables.aimachines.draw(
-    level.aimachines.name,
-    level.aimachines.x,
-    level.aimachines.y)
- Drawables.gates.draw(level.gates.name,level.gates.x,level.gates.y)
- for y, row in ipairs(level.signator) do
+    Level.aimachines.name,
+    Level.aimachines.x,
+    Level.aimachines.y)
+ Drawables.gates.draw(Level.gates.name,Level.gates.x,Level.gates.y)
+ for _, row in ipairs(Level.signator) do
   Drawables.signator.draw(row.state,row.x,row.y)
  end
 
 
  -- temp debug solutions
-love.graphics.print(mousex,100,10)
-love.graphics.print(mousey,300,10)
+love.graphics.print(Mousex,100,10)
+love.graphics.print(Mousey,300,10)
 
 end
 
-function love.mousepressed(x,y,button)
+function love.Mousepressed(x,y,button)
  if button ==1 then
-  for y,table in ipairs(level.signator) do
-   if table.x == x or table.x +25 >= x and table.y == y or table.y +25 >= y then 
+  for _,table in ipairs(Level.signator) do
+   if table.x == x or table.x +25 >= x and table.y == y or table.y +25 >= y then
     table.state = not table.state
    end
   end
-  mousex = x
-  mousey = y
+  Mousex = x
+  Mousey = y
  end
 end
